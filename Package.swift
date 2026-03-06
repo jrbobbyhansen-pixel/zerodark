8b03e9b23552c1360b50f7f5d9312e0307e33459
Cristopher Bautista <cristopher.abg@icloud.com>
2026-03-06 14:23:47 -0600

    Fix min OS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "MLXEdgeLLM",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v14),
         .visionOS(.v1)
     ],
